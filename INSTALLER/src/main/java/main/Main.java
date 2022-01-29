e32d519ed5b996e7d64f40a4dc8d8ced30aff75a
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 14:53:11 -0500

    Added descriptions

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -1,13 +1,8 @@
 package main;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-        /*
-        TODO: Figure out a way to replace the image icon when you launch the icon, Make a decent register system.
-         */
 
 public class Main implements ActionListener {
     private static JTextField userTextField;
@@ -20,15 +15,15 @@ public class Main implements ActionListener {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        // frame setting
-        frame.setSize(600,800);
+        // THE CORE OF THE SYSTEM!!!
+        frame.setSize(600,800); // (you can change the dimensions from the .setBounds(x,y,w,h)
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         panel.setLayout(null);
 
         // username setting
         label = new JLabel("Username:");
-        label.setBounds(10,20,80,25); // 10 = x, 20 = y, 80 = width, 25 = height.
+        label.setBounds(10,20,80,25); // (you can change the dimensions from the .setBounds(x,y,w,h)
         panel.add(label);
 
         // username text field settings
@@ -36,34 +31,35 @@ public class Main implements ActionListener {
         userTextField.setBounds(100,20,165,25);
         panel.add(userTextField);
 
-        //password settings
+        //Creates the label  that's on the side of the input box (you can change the dimensions from the .setBounds(x,y,w,h)
         passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(10,50,80,25);
         panel.add(passwordLabel);
 
-        // password text field settings
+        // password field
         passwordText = new JPasswordField();
 
+        // creates the input box for password!
         passwordText.setBounds(100,50,165,25);
         panel.add(passwordText);
 
-        // Login Settings
+        // the login button that is very important for everything below  (you can change the dimensions from the .setBounds(x,y,w,h)
         button  = new JButton("Login");
         button.addActionListener(new Main());
         button.setBounds(10,80,80,25);
         panel.add(button);
 
-        // success settings
+        // sets the text proving the input is correct (you can change the dimensions from the .setBounds(x,y,w,h)
         success = new JLabel("");
         success.setBounds(10,110,300,25);
         panel.add(success);
 
-        // incorrect password settings
+        // sets the text proving the input is wrong (you can change the dimensions from the .setBounds(x,y,w,h)
         incorrect = new JLabel("");
         incorrect.setBounds(10,110,300,25);
         panel.add(incorrect);
 
-        // makes the pannel show or not. **VERY IMPORTANT**
+        // DO NOT GET RID OF AS THIS TELLS INTELLIJ WHETHER TO SHOW THE PANEL OR NOT!!!!!
         frame.setVisible(true);
 
     }
@@ -73,16 +69,16 @@ public class Main implements ActionListener {
      *
      * @param e
      */
-    // Action Perform when you click login! (prints text via command line)
+    // Tells us whether when you click the button is correct or not (simple login system where if the password/username is correct)
     @Override
     public void actionPerformed(ActionEvent e) {
         String user = userTextField.getText();
         String password = passwordText.getText();
         System.out.println(user + ", " + password);
         if(user.equals("TwoOneTwo") && password.equals("123")){
-            success.setText("Login Works!");
-        } else if(user.equals("") && password.equals("")){
-            incorrect.setText("Login Incorrect DM Two#6969 for pword reset!");
+            success.setText("Login Works!"); // <------ if you're using a login system this where the Correct would  be.
+        } else if(user.equals("") && password.equals("")){ // kinda broken login system this is just a wip so you can remove this or make a pr for suggestions
+            incorrect.setText(""); // <------ if you're using a login system this where the incorrect would  be.
         }
     }
 }
