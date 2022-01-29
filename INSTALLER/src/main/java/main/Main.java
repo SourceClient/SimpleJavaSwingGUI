81edeed7590beb847790534a93f2207bb49426f7
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 09:36:10 -0500

    Added a input text field

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -1,16 +1,23 @@
 package main;
 
 import javax.swing.*;
-import javax.swing.JFrame;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
         frame.setSize(600,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("Username:");
+        label.setBounds(10,20,80,25);
+        panel.add(label);
+        JTextField userTextField = new JTextField(20);
+        userTextField.setBounds(100,20,165,25);
+        panel.add(userTextField);
         frame.setVisible(true);
-        JPanel panel = new JPanel();
-        // everything above just creates a 600 x 800 panel that has nothing on it!
 
     }
 
