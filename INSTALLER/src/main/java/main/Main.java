e32263765c4ef91475ee8ec677d8ae47b07d6dbf
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 09:09:07 -0500

    Initial commit

---
+++ INSTALLER/src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Main extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args){
+        new Main().setVisible(true);
+    }
+
+    private Main() {
+        super("Installer");
+        setSize(400,400); // 1024, 768, 800 x 600 modern res'
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Install"); // method below creates button and makes it clickable <-
+        add(button); // adds the button on the GUI
+        setLayout(new FlowLayout());
+    }
+
+}
