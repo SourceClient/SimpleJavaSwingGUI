e60bbdb5daaea9db60cc410431ab810aa281c03f
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 10:37:56 -0500

    Woooo this works Lol (gonna mess around with this stuff as well so this wont be the only example!)

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -1,8 +1,16 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main extends JFrame {
+public class Main implements ActionListener {
+    private static JTextField userTextField;
+    private static JLabel label;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
@@ -13,25 +21,35 @@ public class Main extends JFrame {
         panel.setLayout(null);
 
         // username setting
-        JLabel label = new JLabel("Username:");
+        label = new JLabel("Username:");
         label.setBounds(10,20,80,25); // 10 = x, 20 = y, 80 = width, 25 = height.
         panel.add(label);
         // username text field settings
-        JTextField userTextField = new JTextField(20);
+        userTextField = new JTextField(20);
         userTextField.setBounds(100,20,165,25);
         panel.add(userTextField);
 
         //password settings
-        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(10,50,80,25);
         panel.add(passwordLabel);
 
         // password text field settings
-        JPasswordField passwordText = new JPasswordField();
+        passwordText = new JPasswordField();
 
         passwordText.setBounds(100,50,165,25);
         panel.add(passwordText);
 
+        // Login Settings
+        button  = new JButton("Login");
+        button.addActionListener(new Main());
+        button.setBounds(10,80,80,25);
+        panel.add(button);
+
+        // success settings
+        success = new JLabel("");
+        success.setBounds(10,110,300,25);
+        panel.add(success);
 
 
 
@@ -39,4 +57,16 @@ public class Main extends JFrame {
 
     }
 
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userTextField.getText();
+        String password = passwordText.getText();
+        System.out.println(user + ", " + password);
+
+    }
 }
