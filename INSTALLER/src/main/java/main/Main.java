e0bc83cf5faed88fcc22031aa900f2a0801716c5
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 09:43:27 -0500

    Username and password input text field

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -12,11 +12,23 @@ public class Main extends JFrame {
         panel.setLayout(null);
 
         JLabel label = new JLabel("Username:");
-        label.setBounds(10,20,80,25);
+        label.setBounds(10,20,80,25); // 10 = x, 20 = y, 80 = width, 25 = height.
         panel.add(label);
         JTextField userTextField = new JTextField(20);
         userTextField.setBounds(100,20,165,25);
         panel.add(userTextField);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField();
+
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+
+
 
         frame.setVisible(true);
 
