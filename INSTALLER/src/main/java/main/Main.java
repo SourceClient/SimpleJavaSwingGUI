ffd2d34963a2dfcf4130025c4ee3c63e4e37ff0b
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 09:29:56 -0500

    Default Config for JFrame!

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -2,24 +2,16 @@ package main;
 
 import javax.swing.*;
 import javax.swing.JFrame;
-import java.awt.*;
 
 public class Main extends JFrame {
-    private static final long serialVersionUID = 1L;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600,800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        JPanel panel = new JPanel();
+        // everything above just creates a 600 x 800 panel that has nothing on it!
 
-    public static void main(String[] args){
-        new Main().setVisible(true);
-    }
-
-    public Main() {
-        super("Installer");
-        setSize(400,400); // 1024, 768, 800 x 600 modern res'
-        setResizable(false);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        JButton button = new JButton("Install"); // method below creates button and makes it clickable <-
-        add(button); // adds the button on the GUI
-        setLayout(new FlowLayout());
     }
 
 }
