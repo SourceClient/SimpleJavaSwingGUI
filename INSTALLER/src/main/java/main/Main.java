a155b96f0f97f8b98563d423edf439d605b38a13
liquid smokey balls <83801755+LiquidSmokeBalls@users.noreply.github.com>
2022-01-29 10:22:02 -0500

    added indications

--- INSTALLER/src/main/java/main/Main.java
+++ INSTALLER/src/main/java/main/Main.java
@@ -6,22 +6,27 @@ public class Main extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        // frame setting
         frame.setSize(600,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         panel.setLayout(null);
 
+        // username setting
         JLabel label = new JLabel("Username:");
         label.setBounds(10,20,80,25); // 10 = x, 20 = y, 80 = width, 25 = height.
         panel.add(label);
+        // username text field settings
         JTextField userTextField = new JTextField(20);
         userTextField.setBounds(100,20,165,25);
         panel.add(userTextField);
 
+        //password settings
         JLabel passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(10,50,80,25);
         panel.add(passwordLabel);
 
+        // password text field settings
         JPasswordField passwordText = new JPasswordField();
 
         passwordText.setBounds(100,50,165,25);
